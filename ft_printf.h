d3e3ab5f04455bc7caff540a246f204b181bf26e
Valentin OMNES <vomnes@student.42.fr>
2017-01-12 16:25:38 +0100

    *.h

--- ft_printf.h
+++ ft_printf.h
@@ -15,14 +15,23 @@
 
 # include <stdlib.h>
 # include <stdarg.h>
+# include <unistd.h>
 # include <stdio.h>
 # include <wchar.h>
 # include <locale.h>
 # include "./libft/libft.h"
 
 
+typedef enum			e_length
+{
+	no, hh, h, ll, l, j, z
+}						t_length;
+
 typedef	struct	s_args
 {
+	int 			start;
+	int 			end;
+	int 			new_start;
 	char			prefix[3];
 	int				pre_hash;
 	int				pre_zero;
@@ -30,35 +39,33 @@ typedef	struct	s_args
 	int				pre_sign;
 	int				pre_blank;
 	int				ok_width;
-	unsigned int	width;
+	int				width;
 	int				ok_precision;
-	unsigned int	precision;
-	enum
-	{
-		h,
-		hh,
-		l,
-		ll,
-		j,
-		z,
-		no
-	}		length;
-	char		type[2];
+	int				wildcard_width;
+	int				precision;
+	int				wildcard_precision;
+	t_length		length;
+	char		type;
 	int			size;
 	int			value;
 }				t_args;
 
 int			ft_printf(const char *format, ...);
 
-char	*ft_parsing_flag(char **s, t_args *elem);
-char	*ft_parsing_width(char **s, t_args *elem, va_list *args);
-char	*ft_parsing_precision(char **s, t_args *elem, va_list *args);
-char	*ft_parsing_length(char **s, t_args *elem);
+int		ft_parsing_start(const char *s, t_args *elem);
+int		ft_parsing_flag(const char *s, t_args *elem);
+int		ft_parsing_width(const char *s, t_args *elem);
+int		ft_parsing_precision(const char *s, t_args *elem);
+int		ft_parsing_length(const char *s, t_args *elem);
+void	ft_parsing_type(const char *s, t_args *elem);
+int		ft_manage_parsing(const char *s, t_args *elem);
 
-void ft_init_struct(t_args *elem);
-void ft_printf_struct(t_args *elem);
+
+void ft_init_struct(t_args *elem, int *k);
+void ft_printf_struct(t_args *elem, int *k);
 
 int		ft_is_type(int c);
+int		ft_is_flag(int c);
 int		ft_nb_pct(const char *s);
 
 #endif
