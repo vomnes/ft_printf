b32bc410818a5ad0637975ce106e4d868664a859
Valentin OMNES <vomnes@student.42.fr>
2017-01-13 20:00:55 +0100

    up

--- get_value.c
+++ get_value.c
@@ -26,15 +26,16 @@ static uintmax_t ft_get_unsigned(t_args *elem, int *k, va_list *args)
     uintmax_t nbr;
 
     nbr = va_arg(*args, uintmax_t);
+    //ft_itoa_hex(nbr, 'A', k, elem);
     elem[*k].u_nb = nbr;
     if (elem[*k].length == hh)
         return ((unsigned char)nbr);
     else if (elem[*k].length == h)
-        return ((unsigned short int)nbr);
+        return ((unsigned short)nbr);
     else if (elem[*k].length == l)
-        return ((unsigned long int)nbr);
+        return ((unsigned long)nbr);
     else if (elem[*k].length == ll)
-        return ((unsigned long long int)nbr);
+        return ((unsigned long long)nbr);
     else if (elem[*k].length == j)
         return ((uintmax_t)nbr);
     else if (elem[*k].length == z)
@@ -44,19 +45,12 @@ static uintmax_t ft_get_unsigned(t_args *elem, int *k, va_list *args)
 
 void ft_check_type(t_args *elem, int *k, va_list *args)
 {
-    char *my_s;
-    int my_c;
-    wchar_t my_C;
-    wchar_t* my_S;
-
-	if (elem[*k].type == 's')
-		my_s = va_arg(*args, char *);
-	if (elem[*k].type == 'c')
-		ft_putchar(va_arg(*args, int));
-	if (elem[*k].type == 'C')
-    {
-        my_C = va_arg(*args, wchar_t);
-    }
+//	if (elem[*k].type == 's')
+//		my_s = va_arg(*args, char *);
+//	if (elem[*k].type == 'c')
+//		ft_putchar(va_arg(*args, int));
+//	if (elem[*k].type == 'C')
+//        my_C = va_arg(*args, wchar_t);
 	if (elem[*k].type == 'S')
 		ft_putwstr(va_arg(*args, wchar_t*));
 	if (ft_is_signed(elem[*k].type))
@@ -64,11 +58,11 @@ void ft_check_type(t_args *elem, int *k, va_list *args)
 	if (elem[*k].type == 'U' || elem[*k].type == 'u')
 		ft_get_unsigned(elem, k, args);
 	if (elem[*k].type == 'X')
-		ft_itoa_hex(ft_get_unsigned(elem, k, args), 'A'); //Best ft_itoa_hex... without malloc
+		ft_itoa_hex(ft_get_unsigned(elem, k, args), 'A', k, elem);
 	if (elem[*k].type == 'x')
-		ft_itoa_hex(ft_get_unsigned(elem, k, args), 'a');
+		ft_itoa_hex(ft_get_unsigned(elem, k, args), 'a', k, elem);
 	if (elem[*k].type == 'p')
-		ft_itoa_hex(ft_get_unsigned(elem, k, args), 'a');
+		ft_itoa_hex(ft_get_unsigned(elem, k, args), 'a', k, elem);
 	if (elem[*k].type == 'o' || elem[*k].type == 'O')
-		ft_itoa_octal(ft_get_unsigned(elem, k, args));
+		ft_itoa_octal(ft_get_unsigned(elem, k, args), elem, k);
 }
