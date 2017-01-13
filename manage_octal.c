671051d68b9c391e73b79f00061d80abb84d3649
Valentin OMNES <vomnes@student.42.fr>
2017-01-13 19:58:10 +0100

    works

--- manage_octal.c
+++ manage_octal.c
@@ -10,15 +10,15 @@ void ft_octal_basic_one(const char *value, t_args *elem, int *k)
 	{
 		if (elem[*k].pre_hash == 0 || value[0] == '0')
 			while (i++ < elem[*k].width - elem[*k].size)
-				ft_putchar(' ');
+				ft_putchar('!');
 		else
-			while (i++ < elem[*k].width - ((int)ft_strlen(value) + 2))
-				ft_putchar(' ');
+			while (i++ < elem[*k].width - ((int)ft_strlen(value) + 2) + 1)
+				ft_putchar('~');
 	}
     if (elem[*k].ok_precision == 1 && elem[*k].ok_width == 0 &&
         elem[*k].pre_zero == 0 && elem[*k].pre_hash == 0)
         while (i++ < elem[*k].precision - elem[*k].size)
-            ft_putchar('0');
+            ft_putchar('8');
 }
 
 void ft_octal_basic_two(const char *value, t_args *elem, int *k)
@@ -26,12 +26,12 @@ void ft_octal_basic_two(const char *value, t_args *elem, int *k)
     int i;
 
     i = 0;
-    if (elem[*k].ok_precision == 1 && elem[*k].ok_width == 1 &&
-        elem[*k].pre_zero == 0 && elem[*k].pre_hash == 0)
+    if (elem[*k].ok_precision == 1 && elem[*k].ok_width == 1
+        && elem[*k].pre_zero == 0 && elem[*k].pre_hash == 0)
     {
         if (elem[*k].width <= elem[*k].precision)
             while (i++ < elem[*k].precision - elem[*k].size)
-                ft_putchar('0');
+                ft_putchar('$');
         else if (elem[*k].precision <= ft_strlen(value) &&
             elem[*k].end_space == 0)
             while (i++ < elem[*k].width - elem[*k].precision -
@@ -41,10 +41,10 @@ void ft_octal_basic_two(const char *value, t_args *elem, int *k)
         {
             if (elem[*k].end_space == 0)
                 while (i++ < elem[*k].width - elem[*k].precision)
-                    ft_putchar('$');
+                    ft_putchar('_');
             i = 0;
             while (i++ < elem[*k].precision - elem[*k].size)
-                ft_putchar('0');
+                ft_putchar('3');
         }
     }
 }
@@ -56,14 +56,15 @@ void ft_octal_hash(const char *value, t_args *elem, int *k)
     i = 0;
     if (elem[*k].ok_precision == 1 && elem[*k].ok_width == 1 && elem[*k].pre_zero == 0 && elem[*k].pre_hash == 1 && elem[*k].end_space == 1)
         if ((elem[*k].precision < elem[*k].width) && value[0] == '0')
-            ft_putchar('0');
+            ft_putchar('7');
     if (elem[*k].ok_precision == 1 && elem[*k].ok_width == 1 && elem[*k].pre_zero == 0 && elem[*k].pre_hash == 1 && elem[*k].end_space == 0)
     {
-        if (elem[*k].precision < ft_strlen(value))
-            while (i++ < elem[*k].width - elem[*k].precision -
-            (int)ft_strlen(value) - ((int)ft_strlen(value) -
-            elem[*k].precision))
-                ft_putchar(' ');
+        if (elem[*k].precision <= (int)ft_strlen(value))
+            while (i++ < elem[*k].width - (int)ft_strlen(value) - 1)
+                ft_putchar('$');
+        else if (elem[*k].width > elem[*k].precision)
+            while (i++ < elem[*k].width - elem[*k].precision)
+                ft_putchar('_');
         else
         {
             while (i++ < elem[*k].width - elem[*k].precision - (int)ft_strlen(value))
@@ -82,14 +83,14 @@ static void ft_octal_zero_one_bis(const char *value, t_args *elem, int *k)
 
     i = 0;
     if (elem[*k].width <= elem[*k].precision)
-        while (i++ < elem[*k].precision - elem[*k].size - (int)ft_strlen(value)
-        + (elem[*k].end_space == 1 && value[0] == '0' ? 1 : 0))
+        while (i++ < elem[*k].precision - elem[*k].size - (elem[*k].pre_hash ? 1 : 0) - (value[0] != '0' ? 2 : 0))
+            ft_putchar('6'); //1 for prefix maybe need to adapt after (elem[*k].pre_hash ? 1 : 0)
+    else if (value[0] == '0')
+        while (i++ < elem[*k].precision - 1)
             ft_putchar('0');
     else
-    {
-        while (i++ < elem[*k].precision - elem[*k].size - (int)ft_strlen(value))
-            ft_putchar('0');
-    }
+        while (i++ < elem[*k].precision - elem[*k].size - (elem[*k].pre_hash ? 1 : 0) - 2)
+            ft_putchar('.');
 }
 
 void ft_octal_zero_one(const char *value, t_args *elem, int *k)
@@ -100,16 +101,15 @@ void ft_octal_zero_one(const char *value, t_args *elem, int *k)
     if (elem[*k].ok_precision == 1 && elem[*k].ok_width == 0 &&
         elem[*k].pre_zero == 0 && elem[*k].pre_hash == 1)
     {
-        while (i++ < elem[*k].precision - elem[*k].size - (int)ft_strlen(value))
-            ft_putchar('0');
+        while (i++ < elem[*k].precision - (int)ft_strlen(value) - 1)
+            ft_putchar('9');
         if (value[0] == '0' && elem[*k].precision > 0)
-            ft_putchar('0');
+            ft_putchar('8');
     }
     if (elem[*k].ok_precision == 1 && elem[*k].ok_width == 1 &&
         elem[*k].pre_zero == 0 && elem[*k].pre_hash == 1)
             ft_octal_zero_one_bis(value, elem, k);
 }
-
 
 void ft_octal_zero_two(const char *value, t_args *elem, int *k)
 {
@@ -119,19 +119,19 @@ void ft_octal_zero_two(const char *value, t_args *elem, int *k)
     if (elem[*k].pre_zero == 1 && elem[*k].ok_precision == 1 &&
         elem[*k].ok_width == 0)
         while (i++ < elem[*k].precision - elem[*k].size)
-            ft_putchar('0');
+            ft_putchar('3');
     if (elem[*k].pre_zero == 1 && elem[*k].ok_precision == 1 &&
         elem[*k].ok_width == 1)
     {
         if (elem[*k].width <= elem[*k].precision)
             while (i++ < elem[*k].precision - elem[*k].size)
-                ft_putchar('0');
+                ft_putchar('3');
         else
         {
             while (i++ < elem[*k].width - elem[*k].precision)
-                ft_putchar('0');
+                ft_putchar('8');
             while (i++ < elem[*k].precision - elem[*k].size)
-                ft_putchar('0');
+                ft_putchar('9');
         }
     }
 }
@@ -166,7 +166,7 @@ void ft_octal_end_space(const char *value, t_args *elem, int *k)
 int ft_manage_octal(const char *value, t_args *elem, const char *prefix, int *k)
 {
 	ft_adapt_octal(value, elem, k);
-    //ft_octal_basic_one(value, elem, k);
+    ft_octal_basic_one(value, elem, k);
     ft_octal_basic_two(value, elem, k);
     ft_octal_hash(value, elem, k);
 	if (elem[*k].pre_hash == 1 && elem[*k].u_nb != 0)
