c4ce8075ef44c491f3f1b2d34954d7ba4e7b889c
Valentin OMNES <vomnes@student.42.fr>
2017-01-18 12:04:37 +0100

    Add wchar null ok

--- convert_char.c
+++ convert_char.c
@@ -35,7 +35,7 @@ int		ft_print_wchar(wchar_t value, int *k, t_args *elem)
     int i;
 
     i = 0;
-    if ((int)value< 0 || (((int)value > 0xD7FF && (int)value < 0xE000))
+    if ((int)value < 0 || (((int)value > 0xD7FF && (int)value < 0xE000))
     || ((int)value > 0x10FFFF))
         return (-1);
     if (elem[*k].ok_precision == 1)
@@ -43,7 +43,10 @@ int		ft_print_wchar(wchar_t value, int *k, t_args *elem)
     if (elem[*k].end_space == 0 && elem[*k].ok_width == 1)
         while (i++ < elem[*k].width - (int)ft_sizewchar(value))
             ft_putchar((elem[*k].pre_zero == 0) ? ' ' : '0');
-    ft_putwchar_all(value);
+    if (value == 0)
+        ft_putchar(0);
+    else
+        ft_putwchar_all(value);
     i = 0;
     if (elem[*k].end_space == 1 && elem[*k].ok_width == 1)
         while (i++ < elem[*k].width - (int)ft_sizewchar(value))
