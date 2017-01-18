f98fd87511095ddbf5f37889bee6dcafd7e33acc
Valentin OMNES <vomnes@student.42.fr>
2017-01-18 14:38:08 +0100

    fct length

--- ft_printf.c
+++ ft_printf.c
@@ -31,28 +31,56 @@ void ft_printf_area(char const *format, int *i, int *y)
 
 int ft_check_len_wp(t_args *elem, int *k, int *len)
 {
+	int get_pre;
+
+	get_pre = 0;
 	if (elem[*k].ok_width == 1 && elem[*k].ok_precision == 1)
 	{
 		if (elem[*k].width > elem[*k].precision)
 		{
-			if  (elem[*k].width > *len)
+			if (elem[*k].type == 's')
 				return (elem[*k].width);
 			else
-				return (*len);
+			{
+				if  (elem[*k].width > *len)
+					return (elem[*k].width);
+				else
+					return (*len);
+			}
 		}
-		else if (elem[*k].width < elem[*k].precision)
+		else if (elem[*k].width <= elem[*k].precision)
 		{
-			if  (elem[*k].precision > *len)
-				return (elem[*k].precision);
+			if (elem[*k].type == 's' || elem[*k].type == 'S' ||
+			elem[*k].type == 'c' || elem[*k].type == 'C')
+			{
+				if (*len < elem[*k].width)
+					return (elem[*k].width);
+				if (*len < elem[*k].precision)
+					return (*len);
+				else
+					return (elem[*k].precision);
+			}
+			else if (ft_is_signed(elem[*k].type))
+			{
+				get_pre = ((elem[*k].neg == -1) ? 1 : 0) +
+				(((elem[*k].neg != -1) && (elem[*k].pre_sign == 1 ||
+				elem[*k].pre_blank == 1)) ? 1 : 0);
+				if (elem[*k].width == elem[*k].precision)
+					return (elem[*k].width + get_pre);
+				if (*len < elem[*k].precision)
+					return (elem[*k].precision + get_pre);
+				else if (*len < elem[*k].width)
+					return (elem[*k].width + get_pre);
+				else
+					return (*len);
+			}
 			else
-				return (*len);
-		}
-		else if (elem[*k].width == elem[*k].precision)
-		{
-			if  (elem[*k].width > *len)
-				return (elem[*k].width);
-			else
-				return (*len);
+			{
+				if  (elem[*k].precision > *len)
+					return (elem[*k].precision);
+				else
+					return (*len);
+			}
 		}
 	}
 	return (0);
@@ -60,6 +88,11 @@ int ft_check_len_wp(t_args *elem, int *k, int *len)
 
 int ft_check_len_w_p(t_args *elem, int *k, int *len)
 {
+	int get_pre;
+
+	get_pre = ((elem[*k].neg == -1) ? 1 : 0) +
+	(((elem[*k].neg != -1) && (elem[*k].pre_sign == 1 ||
+	elem[*k].pre_blank == 1)) ? 1 : 0);
 	if (elem[*k].ok_width == 1 && elem[*k].ok_precision == 0)
 	{
 		if  (elem[*k].width > *len)
@@ -69,15 +102,54 @@ int ft_check_len_w_p(t_args *elem, int *k, int *len)
 	}
 	if (elem[*k].ok_width == 0 && elem[*k].ok_precision == 1)
 	{
-		if  (elem[*k].precision > *len)
-			return (elem[*k].precision);
+		if (elem[*k].type == 's' || elem[*k].type == 'S' ||
+		elem[*k].type == 'c' || elem[*k].type == 'C')
+		{
+			if (elem[*k].precision > *len)
+				return (*len);
+			else
+				return (elem[*k].precision);
+		}
+		else if (ft_is_signed(elem[*k].type))
+		{
+			if (elem[*k].precision > *len)
+				return (elem[*k].precision + get_pre);
+			else
+				return (*len);
+		}
 		else
-			return (*len);
+		{
+			if  (elem[*k].precision > *len)
+				return (elem[*k].precision);
+			else
+				return (*len);
+		}
 	}
 	if (elem[*k].ok_width == 0 && elem[*k].ok_precision == 0)
+	{
+		if (ft_is_signed(elem[*k].type))
+			return (*len + get_pre);
 		return (*len);
+	}
 	return (0);
 }
+
+int ft_adapt_length(t_args *elem, int *k, int *nb)
+{
+	int less;
+
+	less = 0;
+	if (elem[*k].pre_hash == 1 && (elem[*k].type == 'X' || elem[*k].type == 'x'))
+		if (nb == 0)
+			less++;
+	return (less);
+}
+
+/*	if (elem[*k].pre_hash == 1 && (elem[*k].type == 'X' ||
+	elem[*k].type == 'x' || elem[*k].type == 'u' || elem[*k].type == 'U' ||
+	elem[*k].type == 'p' || ft_is_signed(elem[*k].type)))*/
+// + ((nb < 0) ? 1 : 0) +
+//((nb > 0 && (elem[*k].pre_sign || elem[*k].pre_blank)) ? 1 : 0)
 
 int ft_printf_run(char const *format, t_args *elem, va_list *args, int *len)
 {
@@ -115,12 +187,16 @@ int ft_printf_run(char const *format, t_args *elem, va_list *args, int *len)
     return (*len);
 }
 
+//((elem[k].ok_precision && !elem[k].ok_width && !elem[k].precision && value[0] == '0') ? 1 : 0)
+
 int			ft_printf(const char *format, ...)
 {
     va_list args;
     t_args *elem;
     int len;
 
+	if (format == NULL)
+		return (0);
 	if (*format == '\0')
 		return (0);
     if (!(elem = ft_memalloc(sizeof(t_args) * ft_nb_pct(format))))
