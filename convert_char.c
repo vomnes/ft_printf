9d018bb9b291f151c1a946f23787549833cce24f
Valentin OMNES <vomnes@student.42.fr>
2017-01-18 10:01:42 +0100

    ok_precision == 1 > ok_precision = 0

--- convert_char.c
+++ convert_char.c
@@ -17,6 +17,8 @@ int		ft_print_char(int value, int *k, t_args *elem)
     int i;
 
     i = 0;
+    if (elem[*k].ok_precision == 1)
+        elem[*k].ok_precision = 0;
     if (elem[*k].end_space == 0 && elem[*k].ok_width == 1)
         while (i++ < elem[*k].width - 1)
             ft_putchar((elem[*k].pre_zero == 0) ? ' ' : '0');
@@ -36,6 +38,8 @@ int		ft_print_wchar(wchar_t value, int *k, t_args *elem)
     if ((int)value< 0 || (((int)value > 0xD7FF && (int)value < 0xE000))
     || ((int)value > 0x10FFFF))
         return (-1);
+    if (elem[*k].ok_precision == 1)
+        elem[*k].ok_precision = 0;
     if (elem[*k].end_space == 0 && elem[*k].ok_width == 1)
         while (i++ < elem[*k].width - (int)ft_sizewchar(value))
             ft_putchar((elem[*k].pre_zero == 0) ? ' ' : '0');
