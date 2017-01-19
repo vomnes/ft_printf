63151288a7c8ef4e3c1a885852a4727a5c74ed5b
Valentin OMNES <vomnes@student.42.fr>
2017-01-19 11:39:24 +0100

    Update wstr

--- convert_wstr.c
+++ convert_wstr.c
@@ -66,20 +66,21 @@ static void ft_wstr_pre(const wchar_t *value, int *len, int *k, t_args *elem)
                 (int)ft_sizewchar(value[0]))
                 while (i++ < elem[*k].width - elem[*k].precision +
                 elem[*k].precision)
-                    ft_putchar(space);
+                    ft_putchar_len(space, &elem[*k].arg_len);
             else if ((elem[*k].width - elem[*k].precision) < *len)
-                while (i++ < elem[*k].precision)
-                    ft_putchar(space);
+                while (i++ < elem[*k].precision - *len)
+                    ft_putchar_len(space, &elem[*k].arg_len);
             else if (elem[*k].precision <= *len)
-                while (i++ < elem[*k].width - elem[*k].precision)
-                    ft_putchar(space);
+                while (i++ < elem[*k].width - elem[*k].precision +
+				(*len - elem[*k].precision))
+                    ft_putchar_len(space, &elem[*k].arg_len);
             else
-                while (i++ < elem[*k].width - *len)// + 2)
-                    ft_putchar(space);
+                while (i++ < elem[*k].width - *len)
+                    ft_putchar_len(space, &elem[*k].arg_len);
         }
         else if (elem[*k].ok_precision == 0 && elem[*k].ok_width == 1)
             while (i++ < elem[*k].width - *len)
-                ft_putchar(space);
+                ft_putchar_len(space, &elem[*k].arg_len);
     }
 }
 
@@ -94,14 +95,14 @@ static void ft_wstr_end_space(int *len, int *k, t_args *elem)
         {
             if (elem[*k].precision < *len)
                 while (i++ < elem[*k].width - elem[*k].precision)
-                    ft_putchar(' ');
+                    ft_putchar_len(' ', &elem[*k].arg_len);
             else
                 while (i++ < elem[*k].width - *len)
-                    ft_putchar(' ');
+                    ft_putchar_len(' ', &elem[*k].arg_len);
         }
         else if (elem[*k].ok_precision == 0 && elem[*k].ok_width == 1)
             while (i++ < elem[*k].width - *len)
-                ft_putchar(' ');
+                ft_putchar_len(' ', &elem[*k].arg_len);
     }
 }
 
@@ -116,37 +117,34 @@ int		ft_print_wstr(const wchar_t *value, int *k, t_args *elem)
         len = 6;
     else
     {
-        if (elem[*k].ok_precision == 1)
-            len += (int)ft_wstrnlen(value, elem[*k].precision);
-        else
-            while (value[i] != L'\0')
-                len += (int)ft_sizewchar(value[i++]);
+        while (value[i] != L'\0')
+            len += (int)ft_sizewchar(value[i++]);
+		if (elem[*k].ok_precision == 1 && elem[*k].precision < len)
+	        len = (int)ft_wstrnlen(value, elem[*k].precision);
     }
-//    ft_putnbr(len);
-//    ft_putchar('\n');
-//    ft_putnbr(elem[*k].precision);
-//    ft_putchar('\n');
-//    ft_putnbr(elem[*k].width);
-//    ft_putchar('\n');
-    i = 0;
     ft_wstr_pre(value, &len, k, elem);
-    i = 0;
     if (value != NULL)
     {
         i = 0;
         if (elem[*k].ok_precision == 1)
-            ft_putwstr_size(value, elem[*k].precision);
+			ft_putwstr_size(value, elem[*k].precision);
         else
-            ft_putwstr(value);
+			ft_putwstr(value);
+		elem[*k].arg_len += len;
     }
     else
     {
         if (elem[*k].ok_precision == 1 && elem[*k].precision < len)
-           write(1, "(null)", elem[*k].precision);
+		{
+			write(1, "(null)", elem[*k].precision);
+			elem[*k].arg_len += elem[*k].precision;
+		}
         else
-            ft_putstr("(null)");
+		{
+			ft_putstr("(null)");
+			elem[*k].arg_len += 6;
+		}
     }
-    i = 0;
     ft_wstr_end_space(&len, k, elem);
-    return (len);
+    return (elem[*k].arg_len);
 }
