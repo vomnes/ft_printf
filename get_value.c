a9bf0ff96722aaf040f1f79665243beb0d679165
Valentin OMNES <vomnes@student.42.fr>
2017-01-20 11:30:35 +0100

    get_value.c split fct

--- get_value.c
+++ get_value.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "ft_printf.h"
+#include "ft_printf.h"
 
 static intmax_t ft_get_signed(t_args *elem, int *k, va_list *args)
 {
@@ -54,17 +54,15 @@ static uintmax_t ft_get_unsigned(t_args *elem, int *k, va_list *args)
     return ((unsigned int)nbr);
 }
 
-int ft_check_type(t_args *elem, int *k, va_list *args)
+static void ft_update_type(t_args *elem, int *k, va_list *args)
 {
-    int ret;
-
-    ret = 0;
     if (elem[*k].ok_width == 1 && elem[*k].wildcard_width == 1)
     {
         elem[*k].width = va_arg(*args, int);
         if (elem[*k].width < 0)
             elem[*k].end_space = 1;
-        elem[*k].width = ((elem[*k].width < 0) ? -elem[*k].width : elem[*k].width);
+        elem[*k].width = ((elem[*k].width < 0) ? -elem[*k].width
+        : elem[*k].width);
         elem[*k].ok_width = ((elem[*k].width == 0) ? 0 : 1);
     }
     if (elem[*k].length == l && elem[*k].type == 'c')
@@ -77,31 +75,44 @@ int ft_check_type(t_args *elem, int *k, va_list *args)
         if (elem[*k].type == 's' || elem[*k].type == 'S')
             elem[*k].ok_precision = ((elem[*k].precision < 0) ? 0 : 1);
     }
+}
+
+static void ft_manage_type(t_args *elem, int *k, va_list *args, int *ret)
+{
+    if (elem[*k].type == 'b')
+        *ret = ft_itoa_b(va_arg(*args, unsigned long long int), k, elem);
+    else if (elem[*k].type == 'k')
+        *ret = ft_put_date(va_arg(*args, char *), k, elem);
+    else if (elem[*k].type == '%')
+    	*ret = ft_print_char('%', k, elem);
+    else if (elem[*k].type == 'C')
+        *ret = ft_print_wchar(va_arg(*args, wchar_t), k, elem);
+	else if (elem[*k].type == 'S')
+		*ret = ft_print_wstr(va_arg(*args, wchar_t*), k, elem);
+}
+
+int ft_check_type(t_args *elem, int *k, va_list *args)
+{
+    int ret;
+
+    ret = 0;
+    ft_update_type(elem, k, args);
 	if (elem[*k].type == 's')
 		ret = ft_print_string(va_arg(*args, char *), k, elem);
-	if (elem[*k].type == 'c')
+	else if (elem[*k].type == 'c')
 		ret = ft_print_char(va_arg(*args, int), k, elem);
-    if (elem[*k].type == '%')
-		ret = ft_print_char('%', k, elem);
-	if (elem[*k].type == 'C')
-        ret = ft_print_wchar(va_arg(*args, wchar_t), k, elem);
-	if (elem[*k].type == 'S')
-		ret = ft_print_wstr(va_arg(*args, wchar_t*), k, elem);
-	if (ft_is_signed(elem[*k].type))
+	else if (ft_is_signed(elem[*k].type))
 		ret = ft_itoa_signed(ft_get_signed(elem, k, args), k, elem);
-	if (elem[*k].type == 'U' || elem[*k].type == 'u')
+	else if (elem[*k].type == 'U' || elem[*k].type == 'u')
 		ret = ft_itoa_unsigned(ft_get_unsigned(elem, k, args), k, elem);
-	if (elem[*k].type == 'X')
+	else if (elem[*k].type == 'X')
 		ret = ft_itoa_hex(ft_get_unsigned(elem, k, args), 'A', k, elem);
-	if (elem[*k].type == 'x')
+	else if (elem[*k].type == 'x')
 		ret = ft_itoa_hex(ft_get_unsigned(elem, k, args), 'a', k, elem);
-	if (elem[*k].type == 'p')
+	else if (elem[*k].type == 'p')
 		ret = ft_itoa_ptr(va_arg(*args, unsigned long long int), k, elem);
-	if (elem[*k].type == 'o' || elem[*k].type == 'O')
+	else if (elem[*k].type == 'o' || elem[*k].type == 'O')
 		ret = ft_itoa_octal(ft_get_unsigned(elem, k, args), elem, k);
-    if (elem[*k].type == 'b')
-		ret = ft_itoa_b(va_arg(*args, unsigned long long int), k, elem);
-    if (elem[*k].type == 'k')
-    	ret = ft_put_date(va_arg(*args, char *), k, elem);
+    ft_manage_type(elem, k, args, &ret);
     return (ret);
 }
