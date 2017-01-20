7d2f3a805597d49ba3ede223c46f1405c19a4c41
Valentin OMNES <vomnes@student.42.fr>
2017-01-20 15:07:10 +0100

    Norme useful functions (2files)

---
+++ ft_is.c
@@ -0,0 +1,31 @@
+#include "ft_printf.h"
+
+int		ft_is_type(int c)
+{
+	return (c == 's' || c == 'S' || c == 'p' || c == 'd' || c == 'D' ||
+			c == 'i' || c == 'o' || c == 'O' || c == 'u' || c == 'U' ||
+			c == 'x' || c == 'X' || c == 'c' || c == 'C' || c == '%' ||
+			c == 'b' || c == 'k');
+}
+
+int		ft_is_type_less_pct(int c)
+{
+	return (c == 's' || c == 'S' || c == 'p' || c == 'd' || c == 'D' ||
+			c == 'i' || c == 'o' || c == 'O' || c == 'u' || c == 'U' ||
+			c == 'x' || c == 'X' || c == 'c' || c == 'C');
+}
+
+int		ft_is_flag(int c)
+{
+	return (c == '#' || c == '0' || c == '-' || c == '+' || c == ' ');
+}
+
+int		ft_is_signed(int c)
+{
+	return (c == 'D' || c == 'd' || c == 'i');
+}
+
+int		ft_is_length(int c)
+{
+	return (c == 'h' || c == 'l' || c == 'j' || c == 'z');
+}
--- useful.c
+++ useful.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "ft_printf.h"
+#include "ft_printf.h"
 
 void ft_init_struct(t_args *elem, int *k)
 {
@@ -70,83 +70,4 @@ int	ft_nb_pct(const char *s)
 		i++;
 	}
 	return (count);
-}
-
-int		ft_is_type(int c)
-{
-	return (c == 's' || c == 'S' || c == 'p' || c == 'd' || c == 'D' ||
-			c == 'i' || c == 'o' || c == 'O' || c == 'u' || c == 'U' ||
-			c == 'x' || c == 'X' || c == 'c' || c == 'C' || c == '%' ||
-			c == 'b' || c == 'k');
-}
-
-int		ft_is_type_less_pct(int c)
-{
-	return (c == 's' || c == 'S' || c == 'p' || c == 'd' || c == 'D' ||
-			c == 'i' || c == 'o' || c == 'O' || c == 'u' || c == 'U' ||
-			c == 'x' || c == 'X' || c == 'c' || c == 'C');
-}
-
-int		ft_is_flag(int c)
-{
-	return (c == '#' || c == '0' || c == '-' || c == '+' || c == ' ');
-}
-
-int		ft_is_signed(int c)
-{
-	return (c == 'D' || c == 'd' || c == 'i');
-}
-
-int		ft_is_length(int c)
-{
-	return (c == 'h' || c == 'l' || c == 'j' || c == 'z');
-}
-
-void ft_printf_struct(t_args *elem, int *k)
-{
-    ft_putstr("#:  ");
-    ft_putnbr(elem[*k].pre_hash);
-    ft_putchar('\n');
-    ft_putstr("0:  ");
-    ft_putnbr(elem[*k].pre_zero);
-    ft_putchar('\n');
-    ft_putstr("-:  ");
-    ft_putnbr(elem[*k].end_space);
-    ft_putchar('\n');
-    ft_putstr("+:  ");
-    ft_putnbr(elem[*k].pre_sign);
-    ft_putchar('\n');
-    ft_putstr(" :  ");
-    ft_putnbr(elem[*k].pre_blank);
-    ft_putchar('\n');
-    ft_putstr("ok_width:  ");
-    ft_putnbr(elem[*k].ok_width);
-    ft_putchar('\n');
-    ft_putstr("width:  ");
-    ft_putnbr(elem[*k].width);
-    ft_putchar('\n');
-	ft_putstr("wildcard_width:  ");
-	ft_putnbr(elem[*k].wildcard_width);
-	ft_putchar('\n');
-    ft_putstr("ok_precision:  ");
-    ft_putnbr(elem[*k].ok_precision);
-    ft_putchar('\n');
-    ft_putstr("precision:  ");
-    ft_putnbr(elem[*k].precision);
-	ft_putchar('\n');
-	ft_putstr("wildcard_precision:  ");
-	ft_putnbr(elem[*k].wildcard_precision);
-	ft_putchar('\n');
-	ft_putstr("-->start:  ");
-	ft_putnbr(elem[*k].start);
-	//ft_putchar('\n');
-	ft_putstr(" end:  ");
-	ft_putnbr(elem[*k].end);
-	//ft_putchar('\n');
-	ft_putstr(" --->type:  ");
-	ft_putchar(elem[*k].type);
-	ft_putchar('\n');
-	//ft_putstr("length:  ");
-	//ft_putnbr(elem[*k].length);
-	//ft_putchar('\n');
 }
